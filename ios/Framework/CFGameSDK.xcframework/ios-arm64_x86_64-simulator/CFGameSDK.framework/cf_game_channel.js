5fb9ed90247c76eb0d666906b1ef0a9c8ac1da31
Lance <347706549.qq.com>
2025-11-07 23:37:45 +0800

    修复sdk错误字段

--- ios/Framework/CFGameSDK.xcframework/ios-arm64_x86_64-simulator/CFGameSDK.framework/cf_game_channel.js
+++ ios/Framework/CFGameSDK.xcframework/ios-arm64_x86_64-simulator/CFGameSDK.framework/cf_game_channel.js
@@ -50,7 +50,7 @@ var cf_game={
         openChargePage: function () {
             var invokeId = getInvokeId();
             console.log("sdk openChargePage");
-            CFGameOpenApi.openChargePage.postMeERRssage(invokeId);
+            CFGameOpenApi.openChargePage.postMessage(invokeId);
         },
 
         closeGamePage: function () {
